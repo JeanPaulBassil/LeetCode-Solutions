3433ab25e67208711eb8d88d8e2b1fa80efde032
Jean-Paul <Jean.paul.bassil@outlook.com>
2023-03-31 14:46:00 +0300

    added Maximum Subarray Medium DP problem

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -1,2 +1,19 @@
 public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        int len = nums.length;
+        if (len == 1) return nums[0];
+        int max = nums[0];
+        for (int i = 1; i < len; i++) {
+            if (nums[i - 1] > 0) nums[i] += nums[i - 1];
+            max = Math.max(max, nums[i]);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        MaximumSubarray ms = new MaximumSubarray();
+        System.out.println(ms.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+        System.out.println(ms.maxSubArray(new int[]{1}));
+        System.out.println(ms.maxSubArray(new int[]{5, 4, -1, 7, 8}));
+    }
 }
